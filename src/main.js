8eb0b2fb6c7994926ddf67788ad7760fa9d1c4e1
Eugene Martinez <eugenejrmartinez@gmail.com>
2025-02-22 21:39:44 +0800

    Reorganized project structure into modules

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { AutoShooterGame } from './classes/Game.js';
 import { Enemy } from './classes/Enemy.js';
 import { Boss } from './classes/Boss.js';
-import { Bullet } from './classes/Bullet.js';
 import { config } from './config.js';
 
 new Phaser.Game(config);
